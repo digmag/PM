a2a6847a36039c2a2e2bb6d2a1f01b11aa82bee4
Dmitriy Slabukhin <dmitryslabukhin@gmail.com>
2023-12-27 16:42:34 +0700

    Сделана возможность добавлять консультации на клиенте

--- scripts/inspect_create.js
+++ scripts/inspect_create.js
@@ -39,25 +39,74 @@ function date_time(date){
 document.querySelector("#NotFirstInspection").addEventListener("change", (e)=>{
     if(e.target.checked){
         document.querySelector("#OtherInspections").classList.remove("d-none");
+        $.ajax({
+            url: `https://mis-api.kreosoft.space/api/patient/${localStorage.getItem("patientid")}/inspections/search`,
+            method: "GET",
+            contentType:"application/json",
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            },
+            success: function(data){
+                console.log(data)
+                data.forEach(element => {
+                    document.querySelector("#OtherInspections").innerHTML +=`
+                    <option>${date_time(element.date)} ${element.diagnosis.name} ${element.diagnosis.code}</option>
+                    `;
+                });
+            }
+        });
     }
     else{
         document.querySelector("#OtherInspections").classList.add("d-none");
         document.querySelector("#OtherInspections").innerHTML = "";
     }
-    $.ajax({
-        url: `https://mis-api.kreosoft.space/api/patient/${localStorage.getItem("patientid")}/inspections/search`,
-        method: "GET",
-        contentType:"application/json",
-        headers: {
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        },
-        success: function(data){
-            console.log(data)
-            data.forEach(element => {
-                document.querySelector("#OtherInspections").innerHTML +=`
-                <option>${date_time(element.date)} ${element.diagnosis.name} ${element.diagnosis.code}</option>
-                `;
-            });
-        }
-    });
 });
+
+document.querySelector("#NeedConsultation").addEventListener("change", (e)=>{
+    if(e.target.checked){
+        document.querySelector("#Spetiality").readOnly = false;
+        document.querySelector("#Comment").readOnly = false;
+        document.querySelector("#AddConsultation").disabled = false;
+    }
+    else{
+        document.querySelector("#Spetiality").readOnly = true;
+        document.querySelector("#Comment").readOnly = true;
+        document.querySelector("#AddConsultation").disabled = true;
+        document.querySelector("#Spetiality").value = "";
+        document.querySelector("#Comment").value = "";
+    }
+});
+
+document.querySelector("#Spetiality").addEventListener("input",(e)=>{
+    document.querySelector("#dataspet").innerHTML = "";
+    if(e.target.value =="" && e.target.getAttribute("sid")){
+        e.target.removeAttribute("sid");
+    }
+    else{
+        $.ajax({
+            url:`https://mis-api.kreosoft.space/api/dictionary/speciality?name=${e.target.value}&size=100`,
+            method: "GET",
+            contentType: "application/json",
+            success:function (data){
+                data.specialties.forEach(elem =>{
+                    document.querySelector("#dataspet").innerHTML +=`
+                    <option>${elem.name}</oprion>
+                    `;
+                    e.target.setAttribute("sid", elem.id)
+                });
+            }
+        });
+    }
+});
+
+document.querySelector("#AddConsultation").addEventListener("click", ()=>{
+    let clone = document.querySelector("#consultationDiv").cloneNode(true);
+    clone.querySelector("#com").textContent = document.querySelector("#Comment").value;
+    clone.querySelector("#spec").textContent = document.querySelector("#Spetiality").value;
+    clone.id = "";
+    clone.querySelector("#com").id = "";
+    clone.querySelector("#spec").id = "";
+    clone.classList.remove("d-none");
+    document.querySelector("#Consultations").appendChild(clone);
+});
+
